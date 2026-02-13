e34405b950fe0be788eae9d6c4a64ca8bded303f
sinyakumari6-cell <sinyakumari6@gmail.com>
2026-02-13 16:48:08 +0500

    create user account file

---
+++ src/main/java/org/example/person2/Transaction.java
@@ -0,0 +1,29 @@
+package org.example.person2;
+import java.time.LocalDate;
+
+public class Transaction {
+    private LocalDate date;
+    private String type;       // "Income" or "Expense"
+    private String category;
+    private double amount;
+
+    // Constructor
+    public Transaction(LocalDate date, String type, String category, double amount) {
+        this.date = date;
+        this.type = type;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    // Getters
+    public LocalDate getDate() { return date; }
+    public String getType() { return type; }
+    public String getCategory() { return category; }
+    public double getAmount() { return amount; }
+
+    // Setters (optional)
+    public void setDate(LocalDate date) { this.date = date; }
+    public void setType(String type) { this.type = type; }
+    public void setCategory(String category) { this.category = category; }
+    public void setAmount(double amount) { this.amount = amount; }
+}
