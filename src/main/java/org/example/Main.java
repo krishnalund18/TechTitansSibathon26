f5e42bc56e3257f8d2a2c682dd469be08fe6483b
Ritika Lund <ritulund53@gmail.com>
2026-02-14 10:19:07 +0500

    deleted data models file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,29 @@
-package org.example;
-
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        StorageManager.initialize();
-
-        UserFinancialData data =
-                StorageManager.getData();
-
-        if(data.transactions == null){
-
-            data.transactions = new ArrayList<>();
-
-        }
-
-        data.income = 50000;
-
-        data.transactions.add(
-                new Transaction("Rent",-15000,"2026-03-01")
-        );
-
-        StorageManager.autoSave();
-
-    }
-}
+//package org.example;
+//
+//import java.util.*;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        StorageManager.initialize();
+//
+//        UserFinancialData data =
+//                StorageManager.getData();
+//
+//        if(data.transactions == null){
+//
+//            data.transactions = new ArrayList<>();
+//
+//        }
+//
+//        data.income = 50000;
+//
+//        data.transactions.add(
+//                new Transaction("Rent",-15000,"2026-03-01")
+//        );
+//
+//        StorageManager.autoSave();
+//
+//    }
+//}
